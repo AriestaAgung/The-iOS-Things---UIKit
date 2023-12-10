e6e65c2a9b3e7d3a0fcdcc84f68854b20841cc67
Ariesta <ariesta.work@gmail.com>
2023-12-10 12:54:30 +0800

    Initial Commit

---
+++ The iOS Things/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The iOS Things
+//
+//  Created by Ariesta APP on 10/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
