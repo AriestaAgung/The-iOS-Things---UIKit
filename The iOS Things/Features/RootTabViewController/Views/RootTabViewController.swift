0533cf0fd26510b4aa7057e1feb1ed213257a7f3
Ariesta <ariesta.work@gmail.com>
2023-12-10 13:09:47 +0800

    Delete Main Storyboard

---
+++ The iOS Things/Features/RootTabViewController/Views/RootTabViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RootTabViewController.swift
+//  The iOS Things
+//
+//  Created by Ariesta APP on 10/12/23.
+//
+
+import UIKit
+
+class RootTabViewController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
